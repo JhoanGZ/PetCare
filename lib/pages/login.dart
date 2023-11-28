52f44b274e47dd9f7fef4d1f1858b9673fde71be
Francisco <panxo_22_12@hotmail.com>
2023-11-28 17:57:22 -0300

    se borraron codigo comentado

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,33 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:petcare_app/design/themes.dart';
 import 'package:petcare_app/widgets/checkbox.dart';
-// class LogInPage extends StatelessWidget {
-//   const LogInPage({super.key});
-//   @override
-//   State<LogInPage> createState() => _LogInPageState();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: implement build
-//     throw UnimplementedError();
-//   }
-// }
-
-// class _LogInPageState extends State<LogInPage>{
-//   @override
-//   Widget build(BuildContext context){
-//     return const Scaffold(
-//       body: Center (
-//         child: Column([
-//           Container(
-//             child: Text
-//           )
-//         ]
-//           ),
-//         ),
-//     );
-//   }
-// }
 class LogInPage extends StatelessWidget {
   const LogInPage({super.key});
 
