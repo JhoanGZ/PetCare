d0eaa016888ef800b32ed6df362e8a224a33c3b5
Tyrclaius <tyr_claius@hotmail.com>
2023-12-11 00:57:43 -0400

    refacto app_urls

--- lib/config/app_urls.dart
+++ lib/config/app_urls.dart
@@ -1,4 +1,7 @@
 class AppUrls {
-  static const String baseUrlLocal = 'http://127.0.0.1:8000';
 
+  static const String celUrlConnect = ''; // NOTE: ::FR:: Aquí se deja tu IPv4
+  static const String baseUrl = 'http://127.0.0.1:8000';
+  static const String baseUrlLocal = baseUrl;
+  
 }
